d10859542c171231ed00a68a39abc77468117b2c
liaozhiming <liaozhimingandy@qq.com>
2022-06-28 17:02:35 +0800

    内容: 1.处理db.sqlite3路径为static下,避免因重建容器,导致数据丢失问题; 2.调整entrypoint.sh 文件, 临时解决,否则找不到自定义的app,避免需要手工建表;

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -2,6 +2,7 @@
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
+python manage.py makemigrations resume
 python manage.py migrate
 
 exec "$@"
--- proj_django_resume/settings_for_docker.py
+++ proj_django_resume/settings_for_docker.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'proj_django_resume.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'static/db.sqlite3',
     }
 }
 
